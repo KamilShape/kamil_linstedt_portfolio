da2fccd9f760f5d2620a462dd70e33c3c686bd24
KamilShape <kamilshape@gmail.com>
2022-09-08 20:01:58 +0200

    some styles correstions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
 import {Link, Routes, Route} from "react-router-dom"
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import './App.css';
+import './components/styles/home.css';
+import './components/styles/media.css';
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,3 @@
-import './styles/home.css';
 import Media from './Media.js'
 
 function Home() {
@@ -7,13 +6,13 @@ function Home() {
       <div className="home_wrapper wrapper">
         <div className="home_content">
           <p className="home_description description">Hello, my name is</p>
-          <h1 className="home_title">Kamil Linstedt</h1>
-          <h2 className="home_title">I really like to build websites.</h2>
+          <h1 className="home_title section_title">Kamil Linstedt</h1>
+          <h2 className="home_title section_title">I really like to build websites.</h2>
           <p className="home_description description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta autem ab id itaque aliquid minus perspiciatis ut sed provident quisquam, eligendi minima molestias labore corrupti veritatis eos illo, earum mollitia.</p>
         </div>
         <div className="home_photo-wrapper ">
           <div className="home_photo photo"></div>
-          <Media/>
+          <Media className='home_media'/>
         </div>
       </div>
     </section>
