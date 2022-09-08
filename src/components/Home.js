f1c497991db418b4d448886b4d5d6118313c0f17
KamilShape <kamilshape@gmail.com>
2022-09-08 19:31:41 +0200

    styling nav and header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
-// import './home.css';
+import './styles/home.css';
+import Media from './Media.js'
 
 function Home() {
   return (
     <section className="home section">
-        Home
+      <div className="home_wrapper wrapper">
+        <div className="home_content">
+          <p className="home_description description">Hello, my name is</p>
+          <h1 className="home_title">Kamil Linstedt</h1>
+          <h2 className="home_title">I really like to build websites.</h2>
+          <p className="home_description description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta autem ab id itaque aliquid minus perspiciatis ut sed provident quisquam, eligendi minima molestias labore corrupti veritatis eos illo, earum mollitia.</p>
+        </div>
+        <div className="home_photo-wrapper ">
+          <div className="home_photo photo"></div>
+          <Media/>
+        </div>
+      </div>
     </section>
   );
 }
---
+++ src/components/Media.js
@@ -0,0 +1,12 @@
+import './styles/media.css';
+
+function Home() {
+  return (
+    <section className="media">
+      <div className="media_icon"></div>
+      <div className="media_icon"></div>
+    </section>
+  );
+}
+
+export default Home;
